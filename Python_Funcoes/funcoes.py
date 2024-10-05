99b587742fbef286e14ae6f57b299fc631de747d
luandiniz <luanmacielduartediniz@gmail.com>
2024-10-04 21:57:09 -0300

    Commit 1, adicionado a pasta Pyhton_Funcoes à Python

---
+++ Python_Funcoes/funcoes.py
@@ -0,0 +1,115 @@
+def exibir_mensagem():
+    print("Olá mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem_3(nome="Default"):
+    print(f"Seja bem vindo {nome}!")
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Luan")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Jorge")
+
+##########################################################################################  retorno de funcao
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+print(calcular_total([10, 20, 34])) 
+print(retorna_antecessor_e_sucessor(10))  
+
+########################################################################################## argumentos nomeados
+
+def salvar_carro(marca, modelo, ano, placa):
+    # salva carro no banco de dados...
+    print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
+
+salvar_carro("Volkswagen", "Fusca", 1976, "ABC-1234")
+salvar_carro(marca="Volkswagen", modelo="Fusca", ano=1976, placa="ABC-1234")
+salvar_carro(**{"marca": "Volkswagen", "modelo": "Fusca", "ano": 1976, "placa": "ABC-1234"})
+
+########################################################################################## ags e kwargs
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    texto = "\n".join(args)
+    meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
+    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    print(mensagem)
+
+exibir_poema(
+    "Zen of Python",
+    "Beautiful is better than ugly.",
+    "Explicit is better than implicit.",
+    "Simple is better than complex.",
+    "Complex is better than complicated.",
+    "Flat is better than nested.",
+    "Sparse is better than dense.",
+    "Readability counts.",
+    "Special cases aren't special enough to break the rules.",
+    "Although practicality beats purity.",
+    "Errors should never pass silently.",
+    "Unless explicitly silenced.",
+    "In the face of ambiguity, refuse the temptation to guess.",
+    "There should be one-- and preferably only one --obvious way to do it.",
+    "Although that way may not be obvious at first unless you're Dutch.",
+    "Now is better than never.",
+    "Although never is often better than *right* now.",
+    "If the implementation is hard to explain, it's a bad idea.",
+    "If the implementation is easy to explain, it may be a good idea.",
+    "Namespaces are one honking great idea -- let's do more of those!",
+    autor="Tim Peters",
+    ano=1999,
+)
+
+########################################################################################## parametros apenas por posicao
+
+def criar_carro(modelo, ano, placa, /, marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina") # válido
+criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+
+########################################################################################## parametros apenas por nome
+
+def criar_carro(modelo, ano, placa, /, *, marca, motor, combustivel):
+    print(modelo, ano, placa, marca, motor, combustivel)
+
+criar_carro("Palio", 1999, "ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina") # válido
+criar_carro(modelo="Palio", ano=1999, placa="ABC-1234", marca="Fiat", motor="1.0", combustivel="Gasolina")  # inválido
+
+
+########################################################################################## Objetos de primeira classe
+
+def somar(a, b):
+    return a + b
+
+def exibir_resultado(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"O resultado da operação {a} + {b} = {resultado}")
+
+exibir_resultado(10, 10, somar) 
+
+##########################################################################################  Escopo Local e Global
+
+salario = 2000
+
+def salario_bonus(bonus):
+    global salario
+    salario += bonus
+    return salario
+
+salario_bonus(500)
+
+
+
+
+
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Curso_Python
-Um repositório para guardar meus avanços e aprendizados em Python
